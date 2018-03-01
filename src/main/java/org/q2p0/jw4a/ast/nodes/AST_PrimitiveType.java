5279b603d93a1415d2e33cc8a3881c34ae74426d
Joaquín Luis Monleón Irisarri <joaquinmonleon@gmail.com>
2018-03-01 23:30:31 +0100

    Added a method for get the Class object of an AST_PrimitiveType

--- src/main/java/org/q2p0/jw4a/ast/nodes/AST_PrimitiveType.java
+++ src/main/java/org/q2p0/jw4a/ast/nodes/AST_PrimitiveType.java
@@ -4,23 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum AST_PrimitiveType {
-   BYTE     ("byte"),
-   SHORT    ("short"),
-   INT      ("int"),
-   LONG     ("long"),
-   FLOAT    ("float"),
-   DOUBLE   ("double"),
-   CHAR     ("char"),
-   BOOLEAN  ("boolean");
+   BYTE     ("byte"   , byte.class),
+   SHORT    ("short"  , short.class),
+   INT      ("int"    , int.class),
+   LONG     ("long"   , long.class),
+   FLOAT    ("float"  , float.class),
+   DOUBLE   ("double" , double.class),
+   CHAR     ("char"   , char.class),
+   BOOLEAN  ("boolean", boolean.class);
 
    private final String type;
-   AST_PrimitiveType(final String text) {
+   private final Class _class;
+   AST_PrimitiveType(final String text, final Class c) {
       this.type = text;
+      this._class = c;
    }
 
    @Override public String toString() {
       return type.toLowerCase();
    }
+   public Class toClass() { return _class; }
 
    private static final Map<String, AST_PrimitiveType> lookup = new HashMap<>();
    static {
