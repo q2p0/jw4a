fbb5c522a3dace366f8e27b818ecae42327985b1
DESKTOP-K6U37SS\joaqu <joaquinmonleon@gmail.com>
2018-03-27 17:54:31 +0200

    Minor on Jw4aParserCaller.

--- src/main/java/org/q2p0/jw4a/parser/Jw4aParserCaller.java
+++ src/main/java/org/q2p0/jw4a/parser/Jw4aParserCaller.java
@@ -2,7 +2,7 @@ package org.q2p0.jw4a.parser;
 
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.q2p0.jw4a.ast.AST_TreePrint;
+import org.q2p0.jw4a.ExitErrorCodes;
 import org.q2p0.jw4a.ast.nodes.AST_Package;
 import org.q2p0.jw4a.reflection.ReflectionPaths;
 
@@ -16,9 +16,9 @@ public class Jw4aParserCaller {
         this.paths = paths;
     }
 
-    //TODO: Return AST
     public AST_Package parse() {
 
+        // Prepare the lexer and parser
         Jw4aLexer lexer = new Jw4aLexer( this.input );
         CommonTokenStream tokenStream = new CommonTokenStream( lexer );
         Jw4aParser parser = new Jw4aParser( tokenStream );
@@ -27,14 +27,15 @@ public class Jw4aParserCaller {
         parser.removeErrorListeners();
         parser.addErrorListener(new ParserErrorListener());
 
+        // Parse the input & return the AST_Tree
+        AST_Package ast_root = null;
         try {
-            //AST_Package p = parser.wrappers( this.paths ).root;
-            //AST_TreePrint.print( p, 2 );
-            return parser.wrappers( this.paths ).root;
+            ast_root =  parser.wrappers( this.paths ).root;
         }catch (Exception e){
             System.err.println( e.getMessage() );
-            return null; //TODO: Add an error exit code
+            System.exit(ExitErrorCodes.PARSER_ERROR);
         }
+        return ast_root;
     }
 
 }
